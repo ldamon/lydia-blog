6db2914db74ba9bb45f42895698a8369469b8f65
Lydia Damon <2343392+ldamon@users.noreply.github.com>
2023-02-02 22:35:36 -0500

    Remove it all

--- eleventy.config.js
+++ eleventy.config.js
@@ -132,17 +132,3 @@ module.exports = function(eleventyConfig) {
 		pathPrefix: "/",
 	};
 };
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/images/");
-  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
-
-  return {
-    dir: {
-      input: "src",
-      output: "public"
-    }
-  };
-};
